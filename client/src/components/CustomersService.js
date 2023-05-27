df9a5ebe5b4b1ab26224c48989b9460a99ce4d0c
fourhtyoz <fourhtyoz@gmail.com>
2023-05-27 19:02:50 +0300

    fixed class

--- client/src/components/CustomersService.js
+++ client/src/components/CustomersService.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 const API_URL = 'http://localhost:8000'
 
 export default class CustomerService {
-    constructor(){}
-
     getCustomers() {
         const url = `${API_URL}/api/customers/`
         return axios.get(url).then(response => response.data)
